35417c4599a6a56cb76b97d5943af745134ceeeb
Slowpokita <gricel@gocloud.ai>
2019-12-23 13:30:01 -0300

    First version stable
    First version stable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React, { Fragment } from "react"
+import React, { useState } from "react"
 import { render } from 'react-dom'
 import InputArea from "./js/components/InputArea/InputArea.jsx"
 
 //scss
 import './styles/main.scss'
 
-const App = () => (
-  <div className="gs-body-wrapper flex col center">
-    <p>Write your favorite tags and see the magic when you type "comma"</p>
-    <InputArea/>
-  </div>
-)
+const App = () => {
+
+  const [words, setWords] = useState(["hello", "world"])
+
+  function handleChange (value) {
+    setWords(value)
+  }
+  return (
+    <div className="gs-body-wrapper flex col center">
+      <p>Write your favorite tags and see the magic when you type "comma"</p>
+
+      <InputArea limit={35} preset={words} onChange={(value) => handleChange(value)} id="undesired_words" color="primary"/>
+    </div>
+  )
+}
 
 render(<App/>, document.getElementById('root'))
